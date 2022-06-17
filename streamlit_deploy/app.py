503c6d01509677e48e6f674880981ab6b214e5a4
24GUNV <38719890+24GUNV@users.noreply.github.com>
2022-06-17 14:29:18 +0700

    Updates

--- streamlit_deploy/app.py
+++ streamlit_deploy/app.py
@@ -1,42 +1,133 @@
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
-from icevision.all import *
-from icevision.models import *
+import numpy as np
+import os
 
-from google_drive_downloader import GoogleDriveDownloader as gdd
+import tensorflow as tf
 
-gdd.download_file_from_google_drive(file_id='1WzAf8aiUT1q2b3sfdXvPkY3mK5B_62lj')
+assert tf.__version__.startswith('2')
 
-# Loading in the model
-model_loaded = model_from_checkpoint("/content/drive/MyDrive/AI/garbageclassification.pth")
-model_type = model_loaded["model_type"]
-backbone = model_loaded["backbone"]
-class_map = model_loaded["class_map"]
-img_size = model_loaded["img_size"]
-model = model_loaded["model"]
+tf.get_logger().setLevel('ERROR')
+from absl import logging
 
-img_size = model_loaded["img_size"]
-valid_tfms = tfms.A.Adapter([*tfms.A.resize_and_pad(224), tfms.A.Normalize()])
-# Function to return the predictions
-def predictions(frame):
-  img = PIL.Image.fromarray(frame)
-  pred_dict = model_type.end2end_detect(img, valid_tfms, model, class_map=class_map, detection_threshold=0.5)
-  return pred_dict['detection']
+logging.set_verbosity(logging.ERROR)
+
+# from google_drive_downloader import GoogleDriveDownloader as gdd
+#
+# gdd.download_file_from_google_drive(file_id='1WzAf8aiUT1q2b3sfdXvPkY3mK5B_62lj')
+
+import cv2
+from PIL import Image
+
+model_path = 'model.tflite'
+
+# Load the labels into a list
+classes = ['???'] * model.model_spec.config.num_classes
+label_map = model.model_spec.config.label_map
+for label_id, label_name in label_map.as_dict().items():
+    classes[label_id - 1] = label_name
+
+# Define a list of colors for visualization
+COLORS = np.random.randint(0, 255, size=(len(classes), 3), dtype=np.uint8)
+
+
+def preprocess_image(image, input_size):
+    """Preprocess the input image to feed to the TFLite model"""
+    img = tf.convert_to_tensor(image, dtype=tf.float32)
+    original_image = img
+    resized_img = tf.image.resize(img, input_size)
+    resized_img = resized_img[tf.newaxis, :]
+    resized_img = tf.cast(resized_img, dtype=tf.uint8)
+    return resized_img, original_image
+
+
+def detect_objects(interpreter, image, threshold):
+    """Returns a list of detection results, each a dictionary of object info."""
+
+    signature_fn = interpreter.get_signature_runner()
+
+    # Feed the input image to the model
+    output = signature_fn(images=image)
+
+    # Get all outputs from the model
+    count = int(np.squeeze(output['output_0']))
+    scores = np.squeeze(output['output_1'])
+    classes = np.squeeze(output['output_2'])
+    boxes = np.squeeze(output['output_3'])
+
+    results = []
+    for i in range(count):
+        if scores[i] >= threshold:
+            result = {
+                'bounding_box': boxes[i],
+                'class_id': classes[i],
+                'score': scores[i]
+            }
+            results.append(result)
+    return results
+
+
+def run_odt_and_draw_results(image, interpreter, threshold=0.5):
+    """Run object detection on the input image and draw the detection results"""
+    # Load the input shape required by the model
+    _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+
+    # Load the input image and preprocess it
+    preprocessed_image, original_image = preprocess_image(
+        image,
+        (input_height, input_width)
+    )
+
+    # Run object detection on the input image
+    results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
+
+    # Plot the detection results on the input image
+    original_image_np = original_image.numpy().astype(np.uint8)
+    for obj in results:
+        # Convert the object bounding box from relative coordinates to absolute
+        # coordinates based on the original image resolution
+        ymin, xmin, ymax, xmax = obj['bounding_box']
+        xmin = int(xmin * original_image_np.shape[1])
+        xmax = int(xmax * original_image_np.shape[1])
+        ymin = int(ymin * original_image_np.shape[0])
+        ymax = int(ymax * original_image_np.shape[0])
+
+        # Find the class index of the current object
+        class_id = int(obj['class_id'])
+
+        # Draw the bounding box and label on the image
+        color = [int(c) for c in COLORS[class_id]]
+        cv2.rectangle(original_image_np, (xmin, ymin), (xmax, ymax), color, 2)
+        # Make adjustments to make the label visible for all objects
+        y = ymin - 15 if ymin - 15 > 15 else ymin + 15
+        label = "{}: {:.0f}%".format(classes[class_id], obj['score'] * 100)
+        cv2.putText(original_image_np, label, (xmin, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # Return the final image
+    original_uint8 = original_image_np.astype(np.uint8)
+    return original_uint8
+
 
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+        DETECTION_THRESHOLD = 0.3
 
-        # makes predictions about the current frame
-        detections = predictions(frame)
+        arr = np.array(frame)
 
-        for i in range(len(detections['bboxes'])):
-          left, top, right, bottom = detections['bboxes'][i].xmin,detections['bboxes'][i].ymin,detections['bboxes'][i].xmax,detections['bboxes'][i].ymax
-          img = cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 2)
-          img = cv2.putText(img, "{} [{:.2f}]".format(detections['labels'][i], float(detections['scores'][i])),
-                        (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        (255, 0, 0), 2)
+        # Load the TFLite model
+        interpreter = tf.lite.Interpreter(model_path=model_path)
+        interpreter.allocate_tensors()
 
-        return img
+        # Run inference and draw detection result on the local copy of the original file
+        detection_result_image = run_odt_and_draw_results(
+            arr,
+            interpreter,
+            threshold=DETECTION_THRESHOLD
+        )
+
+        # Show the detection result
+        return detection_result_image
+
 
 webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
