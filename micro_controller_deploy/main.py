88e8056a7fa0b3fb32786bcfc859cfc72f20e7c0
24GUNV <38719890+24GUNV@users.noreply.github.com>
2022-06-26 22:05:11 +0700

    Change from picamera2 to picamera

--- micro_controller_deploy/main.py
+++ micro_controller_deploy/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import os
+import picamera
+import picamera.array
 from tflite_runtime.interpreter import Interpreter
-from picamer2 import *
-from PIL import Image
-import cv2
+from time import sleep
 
 # Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter = Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
 
 corresponding = {
     0: "cardboard",
@@ -15,41 +16,39 @@ corresponding = {
     3: "glass",
     4: "paper"
 }
+
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Initialize the camera
-picam2 = Picamera2()
-picam2.configure(picam2.still_configuration(main={"size": (320, 320)}))
-picam2.start()
-
 detection_threshold = 0.5
 
-try:
-    while True:
-        print("Got to the loop")
-        image = picam2.capture_array("")
+with picamera.PiCamera() as camera:
+    sleep(0.5) # waiting for the camera to warm up
+    try:
+        camera.resolution = (320, 320)
+        while True:
+            output = np.empty((1*320*320*3), dtype=np.uint8)
+            camera.capture(output, 'rgb')
+            output = output.reshape((1, 320, 320, 3))
 
-        print(image)
+            # Create input tensor out of raw features
+            interpreter.set_tensor(input_details[0]['index'], output)
 
-        # Create input tensor out of raw features
-        interpreter.set_tensor(input_details[0]['index'], image)
+            print("Running inference")
+            # Run inference
+            interpreter.invoke()
 
-        print("Running inference")
-        # Run inference
-        interpreter.invoke()
+            # Gets detection outputs
+            detected_scores = np.squeeze(interpreter.get_tensor(output_details[0]['index']))
+            detected_classes = np.squeeze(interpreter.get_tensor(output_details[3]['index']))
 
-        # Gets detection outputs
-        detected_scores = np.squeeze(interpreter.get_tensor(output_details[0]['index']))
-        detected_classes = np.squeeze(interpreter.get_tensor(output_details[3]['index']))
+            classes = []
+            for i, score in enumerate(detected_scores):
+                if score >= detection_threshold:
+                    classes.append((score, corresponding[int(detected_classes[i])]))
+            print(classes)
+            sleep(0.5)
 
-        classes = []
-        for i, score in enumerate(detected_scores):
-          if score >= detection_threshold:
-            classes.append((score, corresponding[int(detected_classes[i])]))
-        print(classes)
-        rawCapture.truncate(0)
-
-except KeyboardInterrupt:
-    print("Bye Bye")
+    except KeyboardInterrupt:
+        print("Bye Bye")
